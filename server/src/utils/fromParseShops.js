0971e535e6eec71375b33defd5c5bc24042b3e5f
kondratykdenys <kondratyk.denys@gmail.com>
2021-01-21 13:32:25 +0200

    api test heroku

--- server/src/utils/fromParseShops.js
+++ server/src/utils/fromParseShops.js
@@ -1,32 +1,32 @@
 const formParseShop = [
-  {
-    name: 'Новус',
-    url: 'https://novus.zakaz.ua',
-    nameSearch: {
-      "buckwheat": "buckwheat/"
-    },
-    prefixUrl: 'ru/categories/',
-    find: {
-      product: 'div.products-box__list-item',
-      name: 'span.product-tile__title',
-      price: 'span.Price__value_caption',
-      weight: 'div.product-tile__weight',
-    },
-  },
-  {
-    name: 'Ашан',
-    url: 'https://auchan.zakaz.ua',
-    nameSearch: {
-      "buckwheat": "buckwheat-auchan/"
-    },
-    prefixUrl: 'ru/categories/',
-    find: {
-      product: 'div.products-box__list-item',
-      name: 'span.product-tile__title',
-      price: 'span.Price__value_caption',
-      weight: 'div.product-tile__weight',
-    },
-  },
+  // {
+  //   name: 'Новус',
+  //   url: 'https://novus.zakaz.ua',
+  //   nameSearch: {
+  //     "buckwheat": "buckwheat/"
+  //   },
+  //   prefixUrl: 'ru/categories/',
+  //   find: {
+  //     product: 'div.products-box__list-item',
+  //     name: 'span.product-tile__title',
+  //     price: 'span.Price__value_caption',
+  //     weight: 'div.product-tile__weight',
+  //   },
+  // },
+  // {
+  //   name: 'Ашан',
+  //   url: 'https://auchan.zakaz.ua',
+  //   nameSearch: {
+  //     "buckwheat": "buckwheat-auchan/"
+  //   },
+  //   prefixUrl: 'ru/categories/',
+  //   find: {
+  //     product: 'div.products-box__list-item',
+  //     name: 'span.product-tile__title',
+  //     price: 'span.Price__value_caption',
+  //     weight: 'div.product-tile__weight',
+  //   },
+  // },
   {
     name: 'Fozzy',
     url: 'https://fozzyshop.ua/ru',
